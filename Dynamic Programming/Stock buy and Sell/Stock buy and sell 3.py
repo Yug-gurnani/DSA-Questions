47a2abf1f4fb3ea02301da205c23a6c33ae82a8e
sam0000-cmd <yug.gurnani091@gmail.com>
2021-08-12 14:12:40 +0530

    Added new problem

---
+++ Dynamic Programming/Stock buy and Sell/Stock buy and sell 3.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        
+        dp = [[0 for i in range(n)] for i in range(3)]
+        
+        for i in range(1,3):
+            maxdiff = -prices[0]
+            for j in range(1,n):
+                
+                dp[i][j] = max(dp[i][j-1], prices[j] + maxdiff)
+                
+                maxdiff = max(maxdiff, dp[i-1][j] - prices[j])
+        # print(dp)
+        return dp[2][n-1]
+
+
+        
